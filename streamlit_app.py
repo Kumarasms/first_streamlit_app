e681efe08f1c556701a27739488c6e27594df03b
Kumara <111491803+Kumarasms@users.noreply.github.com>
2022-08-18 13:44:57 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,7 +2,7 @@ import streamlit
 
 streamlit.title('My New Healthy Diner')
 streamlit.header(' *** Breakfast Menu ***')
-streamlit.text('🥣Omega 3 & Blueberry Oatmeal')
+streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
 streamlit.text(' 🥗 Kale, Spinach & Rocket Smoothie')
-streamlit.text('🐔  Hard-Boiled Free-Range Egg')
+streamlit.text('🐔 Hard-Boiled Free-Range Egg')
 streamlit.text('🥑🍞 Avocado Toast')
